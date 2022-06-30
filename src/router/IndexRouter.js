45a43ecde9ea3451b0d41a00d832262e8d0a004d
Narutosma <1627366524@qq.com>
2022-06-30 10:40:45 +0800

    首次上传项目

---
+++ src/router/IndexRouter.js
@@ -0,0 +1,17 @@
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from '../views/Login';
+import NewsSandBox from '../views/NewsSandBox';
+
+import React from 'react'
+
+export default function IndexRouter() {
+  return (
+    <HashRouter>
+        <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/abc" element={ <Navigate to="/login" replace/>}/>
+            <Route path="/" element={localStorage.getItem('token') ? <NewsSandBox/> : <Navigate to="/login" replace/>} />
+        </Routes>
+    </HashRouter>
+  )
+}
